7e414753e31fec3620f944ec2b6663afa99049a9
Zainuddin Siddiqi <57043176+z-siddiqi@users.noreply.github.com>
2025-03-23 21:36:34 +0000

    integrate google calendar api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,39 @@
+import { JWT } from "google-auth-library";
+import { google } from "googleapis";
+
+const CALENDAR_ID = process.env.CALENDAR_ID!;
+const SERVICE_ACCOUNT_KEY = JSON.parse(process.env.SERVICE_ACCOUNT_KEY!);
+
+
 const server = Bun.serve({
   async fetch(req: Request) {
     if (req.method !== "GET") {
       return new Response("Method not allowed", { status: 405 });
     }
+    
+    const jwt = new JWT({
+      email: SERVICE_ACCOUNT_KEY.client_email,
+      key: SERVICE_ACCOUNT_KEY.private_key,
+      scopes: [
+        "https://www.googleapis.com/auth/calendar",
+        "https://www.googleapis.com/auth/calendar.events",
+      ],
+    });
 
-    return new Response(JSON.stringify({ message: "Hello via Bun!" }), {
+    await jwt.authorize();
+    
+    const client = google.calendar({ version: "v3", auth: jwt });
+
+    await client.events.insert({
+      calendarId: CALENDAR_ID,
+      requestBody: {
+        summary: "Test Event",
+        start: { dateTime: new Date().toISOString(), timeZone: "Europe/London" },
+        end: { dateTime: new Date().toISOString(), timeZone: "Europe/London" },
+      }
+    });
+
+    return new Response(JSON.stringify({ message: "Prayer times created successfully" }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
